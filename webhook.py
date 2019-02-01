d7be24196270527214d34f3380601cd977e1307f
dc430 <deepakc3010@gmail.com>
2019-02-01 22:14:55 +0530

    Initial commit

---
+++ webhook.py
@@ -0,0 +1,36 @@
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+    
+    res = makeResponse(req)
+    
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeResponse(req):
+    name = req.get('name')
+    speech = 'Name:' + name
+    return {
+    "fulfillmentText": speech,
+    "source": "webhook"
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
