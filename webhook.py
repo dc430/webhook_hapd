365a7cd2709fbb932763db1c09e1c33cf674aded
deepak chauhan <deepakc3010@gmail.com>
2019-02-09 20:42:46 +0530

    Update

--- webhook.py
+++ webhook.py
@@ -9,25 +9,26 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-def init_gh_user():
-    return Github("hapd", "mAJORPROJECT19").get_user()
+USER = "hapd"
+PWD = "mAJORPROJECT19"
 
-@app.route('/addNewUser', methods=['POST'])
-def addNewUser():
+@app.route('/addUser', methods=['POST'])
+def addUser():
     req = request.get_json(silent=True, force=True)
-    db = init_gh_user().get_repo("database")
+    gh = Github(USER, PWD)
+    db = gh.get_repo("database")
     users = db.get_contents("users.json")
-    temp = users.decoded_content.decode("utf-8")
+    temp = (users.decoded_content).decode("utf-8")
     temp = json.loads(temp)
     pid = temp["currentPID"]
-    temp[str(pId)] = {
+    temp[str(pid)] = {
         "name": req.get('name'),
         "pin": req.get('pin'),
         "age": req.get('age'),
         "dob": req.get('dob'),
         "gender": req.get('gender')
     }
-    temp["currentPID"] = int(temp["currentPID"])
+    temp["currentPID"] = str(int(temp["currentPID"])+1)
     temp = json.dumps(temp, indent=4)
     try:
         db.update_file(users.path, "Update users", str(temp), users.sha)
@@ -47,25 +48,6 @@ def addNewUser():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-@app.route('/checkLogin', methods=['POST'])
-def checkLogin():
-    req = request.get_json(silent=True, force=True)
-    db = init_gh_user().get_repo("database")
-    users = db.get_contents("users.json")
-    temp = users.decoded_content.decode("utf-8")
-    temp = json.loads(temp)
-    if(temp[req.get('pId')["pin"]] == req.get("pin")):
-        res = {
-            "Login Access": "True"
-        }
-    else:
-        res = {
-            "Login Access": "False"
-        }
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
